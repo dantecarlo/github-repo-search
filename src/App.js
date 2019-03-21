a09b7b6aa879619b1ea6d085e9e740e8708db989
Dante <>
2019-03-21 10:27:52 -0500

    Instalando otros paquetes para apollo: npm install apollo-client apollo-cache-inmemory apollo-link-http react-apollo graphql-tag graphql --save
    Configurando correctamente segundo guia de Apollo la conección al api
    de Github

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 // React
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Apollo
 import { ApolloProvider } from 'react-apollo'
-import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://api.example.com/graphql" }),
+  cache: new InMemoryCache(),
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("networkError", networkError);
+    console.log("graphQLErrors", graphQLErrors);
+  }
+});
+
+/*const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   onError: ({networkError, graphQLErrors}) => {
     console.log('networkError', networkError);
     console.log('graphQLErrors', graphQLErrors);
   }
-})
-
+})*/
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1> Github Users </h1>
-        </header>
-      </div>
-    );
+      <ApolloProvider client={client}>
+        <h1>Github Users</h1>
+      </ApolloProvider>
+    )
   }
 }
 
