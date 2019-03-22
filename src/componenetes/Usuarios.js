8853e2f890b6f77b74b9bc6213d4a2b8eacd34fc
dantecarlo <carlo_cnp@hotmail.com>
2019-03-22 18:47:57 -0500

    Eliminando componentes header y uniendolo con el componente usuario para un manejo de los formularios sin uso de herrmientas extra.
    Se modifico el componenete usuario para soportar las
    busquedas en el formulario de la seccion de Header.

--- src/componenetes/Usuarios.js
+++ src/componenetes/Usuarios.js
@@ -1,11 +1,80 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { Query } from "react-apollo";
 import "./Usuarios.css";
-import Header from "./Header";
 
 import { BUSCAR_USUARIOS_QUERY } from "../queries";
+import { variablesInOperation } from "apollo-utilities";
 
-const Usuarios = () => (
+
+export default class Usuarios extends Component {
+  state = {
+    nombre: ' '
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="header">
+          <div>
+            <h1 className="text-Left title-style">Github Users</h1>
+          </div>
+          <div className="form-style">
+            <form onSubmit={e => {
+              e.preventDefault();
+            }}>
+
+              <input type="search"
+                placeholder="Search Github Users"
+                onChange={e => {
+                  this.setState({
+                    ...this.state,
+                    nombre: e.target.value
+                  })
+                }}
+              />
+
+            </form>
+          </div>
+        </div>
+
+        <div>
+          <Query query={BUSCAR_USUARIOS_QUERY} variables={"john"}>
+            {({ loading, error, data }) => {
+              if (loading) return "Cargando...";
+              if (error) return `Error: ${error.message}`;
+              return (
+                <ul className="list-group list-group-flush">
+                  {data.search.edges.map(item => (
+                    <li key={item.id} className="list-group-item">
+                      <div className="d-inline-block text-center">
+                        <img
+                          key={item.id}
+                          src={item.node.avatarUrl}
+                          className="image rounded-circle"
+                          alt="avatar"
+                        />
+                      </div>
+                      <div className="d-inline-block data">
+                        <span className="name">
+                          {item.node.name}, {item.node.location}
+                        </span>
+                        <br />
+                        <span className="login">{item.node.login}</span>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )
+            }}
+          </Query>
+        </div>
+      </Fragment >
+    )
+  }
+}
+
+
+/*const Usuarios = () => (
   <Query query={BUSCAR_USUARIOS_QUERY}>
     {({ loading, error, data }) => {
       if (loading) return "Cargando...";
@@ -47,3 +116,4 @@ const Usuarios = () => (
 );
 
 export default Usuarios;
+*/
